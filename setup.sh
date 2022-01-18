93e6cd943addadb2ec531fa432d38b0c8d214a9e
迎松 <yanyingsong@outlook.com>
2022-01-18 12:23:27 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 pkg='elf.x64-proctop.tar.gz'
-wget https://github.com/matsuwin/proctop/releases/download/v0.1.9/$pkg
+wget https://github.com/matsuwin/proctop/releases/download/v0.1.10/$pkg
 sudo tar -C /bin -xf $pkg
 rm -f $pkg
