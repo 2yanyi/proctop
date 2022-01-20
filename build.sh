fea873f5eb359ebc5b73cc664eff70e26974f293
Yingsong Yan <you@example.com>
2022-01-20 21:46:41 +0800

    +push

--- build.sh
+++ build.sh
@@ -1,30 +1,40 @@
 #!/usr/bin/env bash
 
 if !(type go >/dev/null 2>&1); then
-  echo 'go: command not found'
-  exit
+    echo 'go: command not found'
+    exit
 fi
 
-# main
 export GOPROXY=https://goproxy.cn
-export GOARCH=amd64
-export GOOS=linux
 gofmt -w .
 
 # build
-static=$1
+if [[ $1 == "dist" ]]; then
 
-if [[ ${static} == "static" ]]; then
+    if !(type pigz >/dev/null 2>&1); then
+        echo 'pigz: command not found'
+        exit
+    fi
 
-  # version
-  version=$(($(cat version)+1)); printf ${version} > version
+    # version++
+    version=$(($(cat version)+1)); printf ${version} > version
 
-  # 静态编译
-  go build -ldflags '-linkmode "external" -extldflags "-static" -X main.BuildID='${version} -o bin/proctop main.go
+    # Windows
+    GOOS=windows GOARCH=386 go build -ldflags '-X main.BuildID='${version} -o bin/proctop.exe main.go
+
+    # Raspberry Pi
+    GOOS=linux GOARCH=arm go build -ldflags '-X main.BuildID='${version} -o proctop main.go
+    tar -cf - proctop | pigz > bin/elf.raspberry-proctop.tar.gz
+
+    # Linux
+    GOOS=linux GOARCH=amd64 go build \
+        -ldflags '-linkmode "external" -extldflags "-static" -X main.BuildID='${version} -o proctop main.go
+    tar -cf - proctop | pigz > bin/elf.x64-proctop.tar.gz
+
+    rm -f proctop
 
 else
 
-  # 普通编译
-  go build -ldflags '-X main.BuildID='${version} -o bin/proctop main.go
+    go build -ldflags '-X main.BuildID='${version} -o bin/proctop main.go
 
 fi
--- flagset/flagset.go
+++ flagset/flagset.go
@@ -1,9 +1,11 @@
 package flagset
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 const (
@@ -13,9 +15,10 @@ const (
 
 var fs = flag.NewFlagSet(binary, flag.ExitOnError)
 var (
-	__version = fs.Bool("version", false, "show version information")
-	Java      = fs.Bool("java", false, "Java process list")
-	Limit     = fs.Int("l", 10, "limit")
+	__version       = fs.Bool("version", false, "show version information")
+	Java            = fs.Bool("java", false, "Java process list")
+	__diskWriteRate = fs.Bool("diskw", false, "Disk write rate Test")
+	Limit           = fs.Int("l", 10, "limit")
 )
 
 func Init(BuildID string) (_ bool) {
@@ -35,5 +38,27 @@ func Init(BuildID string) (_ bool) {
 		return
 	}
 
+	if *__diskWriteRate {
+		fmt.Printf("Write rate %.1fMB/s\n", diskWriteRate()/1024/1024)
+		return
+	}
+
 	return true
+}
+
+func diskWriteRate() float64 {
+	size := 1024 * 1024 * 128
+	buf := bytes.Buffer{}
+	for i := 0; i < size; i++ {
+		buf.Write([]byte{'0'})
+	}
+	start := time.Now()
+	if err := os.WriteFile(".bytes", buf.Bytes(), 0666); err != nil {
+		println("data write error: " + err.Error())
+		return 0
+	}
+	duration := time.Now().Sub(start)
+	second := float64(size) / (float64(duration) / float64(time.Second))
+	_ = os.Remove(".bytes")
+	return second
 }
