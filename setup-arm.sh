3d923dba5e140363a8bb95b5d7b3da3e7a3b6f78
迎松 <yanyingsong@outlook.com>
2022-02-24 16:02:08 +0800

    Update setup-arm.sh

--- setup-arm.sh
+++ setup-arm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pkg='elf.raspberry-proctop.tar.gz'
+pkg='elf.arm-proctop.tar.gz'
 wget https://github.com/matsuwin/proctop/releases/download/v0.1.13/$pkg
 sudo tar -C /bin -xf $pkg
 sudo chmod 0777 /bin/proctop
